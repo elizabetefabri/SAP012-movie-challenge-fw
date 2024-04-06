2797cd9a954759938e8cf27b22e59395a2defd32
Elizabete <elizabete958@gmail.com>
2024-04-06 04:06:08 -0300

    Add componente de paginação na home e suas funcionalidades

--- movie/src/app/layout/home/home.component.spec.ts
+++ movie/src/app/layout/home/home.component.spec.ts
@@ -1,21 +1,91 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { ApiService } from 'src/app/shared/services/api.service';
+import { Movie } from 'src/models/Movie';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MovieListComponent } from '../movie-list/movie-list.component';
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let apiService: ApiService;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let route: ActivatedRoute;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    });
+  beforeEach(async () => {
+    const routerSpyObj = jasmine.createSpyObj('Router', ['navigate']);
+    const routeStub = {
+      queryParams: of({})
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [
+        HomeComponent,
+        MovieListComponent,
+      ],
+      providers: [
+        ApiService,
+        { provide: ActivatedRoute, useValue: routeStub },
+        { provide: Router, useValue: routerSpyObj }
+      ],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiService = TestBed.inject(ApiService);
+    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    route = TestBed.inject(ActivatedRoute);
   });
 
-  it('should create', () => {
+
+  it('deve criar', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve buscar filmes na inicialização', fakeAsync(() => {
+    const movies: Movie[] = [
+      {
+        id: 1,
+        title: 'Movie 1',
+        image_path: 'path/to/image1.jpg',
+        release_year: '2022',
+        genres: ['Action', 'Adventure'],
+        overview: 'Overview do Movie 1',
+        vote_average: 7.5
+      },
+      {
+        id: 2,
+        title: 'Movie 2',
+        image_path: 'path/to/image2.jpg',
+        release_year: '2023',
+        genres: ['Drama', 'Romance'],
+        overview: 'Overview do Movie 2',
+        vote_average: 8.0
+      },
+    ];
+    // spyOn(ApiService, 'getMovies').and.returnValue(
+    //   of({
+    //     filters: { page: 1 },
+    //     metaData: { pagination: { currentPage: 1, totalPages: 1 } },
+    //     movies: movies,
+    //   })
+    // );
+    // expect(movies).toBeTruthy();
+    // component.ngOnInit();
+    // tick(3000);
+    // expect(ApiService.getMovies).toHaveBeenCalled();
+    // expect(component.movies).toEqual(movies);
+  }));
+
+  it('deve navegar para a página correta quando onPageChange for chamado', () => {
+    const page = 3;
+    component.onPageChange(page);
+    expect(routerSpy.navigate).toHaveBeenCalledWith([], {
+      queryParams: { currentPage: page },
+    });
   });
 });
