b00955cc0c48e3d8282d6b755cd6052c70984f30
Elizabete <elizabete958@gmail.com>
2024-04-06 00:41:21 -0300

    Add configuração da primeira versão de cards

--- movie/src/app/layout/movie-card/movie-card.component.spec.ts
+++ movie/src/app/layout/movie-card/movie-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieCardComponent } from './movie-card.component';
+import { Movie } from 'src/models/Movie';
 
 describe('MovieCardComponent', () => {
   let component: MovieCardComponent;
@@ -15,7 +16,27 @@ describe('MovieCardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve exibir os detalhes do filme corretamente', () => {
+    const movie: Movie[] = [
+      {
+      id: 1,
+      title: 'Test Movie',
+      image_path: 'test_image_path.jpg',
+      release_year: '2022',
+      overview: 'Overview do Movie 1',
+      vote_average: 7.5,
+      }
+    ];
+    component.movie = movie[0];
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain(movie[0].title);
+    expect(compiled.querySelector('p').textContent).toContain(movie[0].release_year);
+    expect(compiled.querySelector('img').src).toContain(movie[0].image_path);
   });
 });
--- movie/src/app/layout/movie-card/movie-card.component.ts
+++ movie/src/app/layout/movie-card/movie-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from 'src/models/Movie';
 
 @Component({
   selector: 'app-movie-card',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent {
+  @Input() movie: Movie | undefined;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
