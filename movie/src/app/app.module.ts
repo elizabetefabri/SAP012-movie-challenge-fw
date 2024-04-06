3564a1028319ba830c2d13bbb222409baaaa8b39
Elizabete <elizabete958@gmail.com>
2024-04-06 00:29:42 -0300

    Create component Movie Card

--- movie/src/app/app.module.ts
+++ movie/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './layout/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule,
