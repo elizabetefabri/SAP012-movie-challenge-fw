312c6a9730910ae3caec16daf77eeec7cf72f850
Elizabete <elizabete958@gmail.com>
2024-04-05 23:38:45 -0300

    Função formatMovie com os parametros de objetos do models Movie e testes

---
+++ movie/src/utils/transformers.spec.ts
@@ -0,0 +1,29 @@
+import { formatMovie } from './transformers';
+
+describe('formatMovie', () => {
+ // DEVE RETORNAR OS 5 ELEMENTOS
+ it('deve retornar um objeto com os cinco elementos solicitados', () => {
+  const apiMovieData = {
+    id: 1011985,
+    title: 'Kung Fu Panda 4',
+    poster_path: '/wkfG7DaExmcVsGLR4kLouMwxeT5.jpg',
+    release_date: '2024-03-02',
+    genre_ids: ['Action', 'Adventure', 'Animation', 'Comedy', 'Family'],
+    overview: 'Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.',
+    vote_average: 6.8
+  };
+
+
+  const formattedMovie = formatMovie(apiMovieData);
+  expect(formattedMovie).toEqual({
+    id: 1011985,
+    title: 'Kung Fu Panda 4',
+    image_path: 'https://image.tmdb.org/t/p/w500/wkfG7DaExmcVsGLR4kLouMwxeT5.jpg',
+    release_year: '2024',
+    genres: ['Action', 'Adventure', 'Animation', 'Comedy', 'Family'],
+    overview: 'Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.',
+    vote_average: 6.8
+  });
+
+});
+});
---
+++ movie/src/utils/transformers.ts
@@ -0,0 +1,17 @@
+import { Movie } from "src/models/Movie";
+
+export function formatMovie(apiMovieData: any): Movie{
+  const baseUrl = 'https://image.tmdb.org/t/p/w500'; // URL base da API TMDB para imagens
+
+  const formattedMovie: Movie = {
+    id: apiMovieData.id,
+    title: apiMovieData.title,
+    image_path: apiMovieData.poster_path ? `${baseUrl}${apiMovieData.poster_path}` : 'https://image.tmdb.org/t/p/w500/wkfG7DaExmcVsGLR4kLouMwxeT5.jpg',
+    release_year: apiMovieData.release_date ? new Date(apiMovieData.release_date).getFullYear().toString() : 'Desconhecido',
+    overview: apiMovieData.overview,
+    vote_average: apiMovieData.vote_average,
+    genres: apiMovieData.genre_ids
+  };
+
+  return formattedMovie;
+}
