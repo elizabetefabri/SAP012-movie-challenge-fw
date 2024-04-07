d8a0d9374c644df14f392bcb618769d94e1b47ec
Elizabete <elizabete958@gmail.com>
2024-04-07 17:51:08 -0300

    add path movie/:id

--- movie/src/app/app-routing.module.ts
+++ movie/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
+import { MovieDetailComponent } from './layout/movie-detail/movie-detail.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent },
+  {path: 'movie/:id', component: MovieDetailComponent}
 ];
 
 @NgModule({
